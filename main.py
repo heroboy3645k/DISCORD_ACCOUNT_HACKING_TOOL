48b36264cbb4993213127033095aa5ee051b9a88
Tems <79542785+Tems-py@users.noreply.github.com>
2022-01-07 00:31:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,32 @@
-
 import client
+
 commands = [
 
-        ("servers", "Display your servers list (names and ids)."), 
-        ("friends", "Display your dm channels (ids)"),
-        ("get_server <server id>", "Display every channels (ids and names)."),
-        ("get_server_users", "Display every users from a server"),
-        ("get_channel <channel id>", "Display messages on channel. Also display who sent message."),
-        ("save <channel_id>", "It saves everything from channel."),
-        
-        ("search <someone id>", "It shows everything program knows about given user."),
-        ("grab_emails", "It shows messages wich propably have emails inside. (looking only for saved messages)"),
+    ("servers", "Display your servers list (names and ids)."),
+    ("friends", "Display your dm channels (ids)"),
+    ("get_server <server id>", "Display every channels (ids and names)."),
+    ("get_server_users", "Display every users from a server"),
+    ("get_channel <channel id>", "Display messages on channel. Also display who sent message."),
+    ("save <channel_id>", "It saves everything from channel."),
 
-        ("send <server_id> <channel_id> <message>", "It sends message to given channel"),
-        ("automatic_send <server_id> <channel_id> <messages> <time>", "It send messages automaticly with choosen brake <time> (only to servers)"),
-        ("create_dm", "creates channel with friend")
+    ("search <someone id>", "It shows everything program knows about given user."),
+    ("grab_emails", "It shows messages which probably have emails inside. (looking only for saved messages)"),
 
+    ("send <server_id> <channel_id> <message>", "It sends message to given channel"),
+    ("automatic_send <server_id> <channel_id> <messages> <time>",
+     "It send messages automatically with chosen brake <time> (only to servers)"),
+    ("create_dm", "creates channel with friend")
 
 ]
+
 
 def print_commands():
     for i in commands:
         print("command: ", i[0], " - ", "Description: ", i[1], "\n")
 
+
 def main():
-
-    print("Welcome in Illegal Discord client") #welcome message
-
+    print("Welcome in Illegal Discord client")  # welcome message
 
     AUTH = input("your discord authorization TOKEN\n")
 
@@ -52,13 +52,7 @@ def main():
         elif command[0] == "send":
             print(command)
             c.send(command[1], command[2])
-            
 
-    
+
 if __name__ == "__main__":
-   main()
-
-
-
-
-        
+    main()
